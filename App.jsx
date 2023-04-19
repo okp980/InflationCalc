870106c4edfd4b4fddd94778cb50c65c173e584b
Emmanuel Okpunor <e.okpunor@clanelaglp018s-MacBook-Pro.local>
2023-04-19 01:19:57 +0100

    clean up

--- App.jsx
+++ App.jsx
@@ -11,8 +11,6 @@ import Crashes from 'appcenter-crashes';
 import Analytics from 'appcenter-analytics';
 import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
-import {Colors} from 'react-native/Libraries/NewAppScreen';
-
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -138,41 +136,5 @@ const styles = StyleSheet.create({
     borderColor: 'gray',
     borderWidth: 1,
     marginTop: 10,
-  },
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
   },
 });
